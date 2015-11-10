16ebdcb992428727db4bc407b7ee5e40c90686cc
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-11-10 11:55:48 +0100

    style(frontend): restyle home page

--- public/app/controllers/home.controller.js
+++ public/app/controllers/home.controller.js
@@ -1,3 +1,28 @@
 angular.module('dmsApp')
-  .controller('HomeCtrl', ['$scope', function($scope){
+  .controller('HomeCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+    var self = this;
+    self.isOpen = false;
+  
+    $scope.$watch('demo.isOpen', function(isOpen) {
+      if (isOpen) {
+        $timeout(function() {
+          $scope.tooltipVisible = self.isOpen;
+        }, 600);
+      } else {
+        $scope.tooltipVisible = self.isOpen;
+      }  
+    });
+    self.items = [
+      {
+        name: 'Signup',
+        icon: '../assets/imgs/account-plus.svg',
+        href: '/#/signup'
+      },
+      {
+        name: 'Login',
+        icon: '../assets/imgs/login.svg',
+        href: '/#/login'
+      }
+    ];
+
   }]);
