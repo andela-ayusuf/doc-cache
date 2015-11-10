4d6b0de5a810217d43dd128cc5526a3932f00416
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-11-10 11:54:41 +0100

    style(frontend): restyle dashboard nav-bar

--- public/app/controllers/dashboard.controller.js
+++ public/app/controllers/dashboard.controller.js
@@ -1,3 +1,23 @@
 angular.module('dmsApp')
-  .controller('DashboardCtrl', ['$scope', function($scope) {
+  .controller('DashboardCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+    var self = this;
+    self.isOpen = false;
+  
+    $scope.$watch('demo.isOpen', function(isOpen) {
+      if (isOpen) {
+        $timeout(function() {
+          $scope.tooltipVisible = self.isOpen;
+        }, 600);
+      } else {
+        $scope.tooltipVisible = self.isOpen;
+      }  
+    });
+    self.items = [
+      {
+        name: 'Profile',
+        icon: '../assets/imgs/account.svg',
+        href: '/#/profile'
+      }
+    ];
+
   }]);
