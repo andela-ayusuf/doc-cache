cc7dbc9280c8ee16b117c77e1929ed57419ab34e
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-11-25 16:23:47 +0100

    chore(backend): db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   secret: 'karishika',
   port: process.env.PORT || 5000,
-  database: 'localhost:27017/apidb'
+  database: 'test:test@ds061691.mongolab.com:61691/abdulmalik'
 };
